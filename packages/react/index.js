517b11a3372d2d171599a1450582002437fc414e
ponday-dev <ponday.dev+github@gmail.com>
2019-11-04 22:22:23 +0900

    add react config

---
+++ packages/react/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        }
+    },
+    plugins: [
+        'react',
+        'react-hooks',
+        'jsx-a11y'
+    ]
+}
